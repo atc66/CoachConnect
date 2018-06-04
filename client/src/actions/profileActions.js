654b9aa5b86c61c808b62525d538c4c540a8e74b
aciukurescu <andrewciukurescu@gmail.com>
2018-06-04 12:52:55 -0400

    delete account and edit profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,7 +3,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from "./types";
 
 // Get current profile
@@ -45,4 +46,24 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   };
+};
+
+//Delete Account&Profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure you want to delete your account?")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
