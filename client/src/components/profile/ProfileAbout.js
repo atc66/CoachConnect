67a92fa5e75b697b67fdd5fa9412a3925da2f472
aciukurescu <andrewciukurescu@gmail.com>
2018-06-04 20:36:53 -0400

    profile by handle and sub components

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProfileAbout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TODO Profile Aout</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;
